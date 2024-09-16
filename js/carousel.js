3e1c35d5ca984f78cee601ba9021d0014cba154c
SergioKnows <foxy.sergiog@gmail.com>
2024-09-15 20:05:15 -0500

    imagesLoaded Library

--- js/carousel.js
+++ js/carousel.js
@@ -1,14 +1,18 @@
 /* Script para crear y configurar el carrusel con Splide js */
 window.addEventListener("load", () => {
-    new Splide('#images-carousel', {
-        type: 'loop',
-        pagination: true,
-        drag: true,
-        speed: 800,
-        gap: 5,
-        autoplay: true,
-        interval: 2500,
-        perPage: 1,
-        perMove: 1,
-    }).mount();
+    const carouselElement = document.querySelector('#images-carousel');
+    imagesLoaded(carouselElement, () => {
+        new Splide(carouselElement, {
+            type: 'loop',
+            pagination: true,
+            drag: true,
+            speed: 800,
+            gap: 5,
+            autoplay: true,
+            interval: 2500,
+            perPage: 1,
+            perMove: 1,
+        }).mount();
+    });
 });
+
